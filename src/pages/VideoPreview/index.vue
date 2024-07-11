5207be9ebb16e9bdc6b3a8e14b8a66773e55ee1f
stwang <464905943@qq.com>
2024-07-11 13:54:23 +0800

    fix: 视频高度100vh播放黑边偏移问题

--- src/pages/VideoPreview/index.vue
+++ src/pages/VideoPreview/index.vue
@@ -1,6 +1,12 @@
 <template>
   <view class="video-preview">
-    <video class="video-box" :src="videoUrl" controls autoplay></video>
+    <video
+      class="video-box"
+      :src="videoUrl"
+      :style="{ height: videoHeight + 'px' }"
+      controls
+      autoplay
+    ></video>
   </view>
 </template>
 
@@ -9,6 +15,15 @@ import { ref } from "vue";
 import { onLoad } from "@dcloudio/uni-app";
 
 const videoUrl = ref("");
+const height =
+  uni.getStorageSync("screenHeight") || uni.getSystemInfoSync().screenHeight;
+// TODO: video 100vh显示总有黑边偏移，暂时的解决方案
+const videoHeight = height - 100;
+
+uni.setStorage({
+  key: "screenHeight",
+  data: height
+});
 
 onLoad((option) => {
   // 支持safari浏览器播放
@@ -17,9 +32,12 @@ onLoad((option) => {
 </script>
 
 <style lang="scss" scoped>
-.video-preview,
-.video-box {
+.video-preview {
   width: 100%;
   height: 100%;
+  background-color: #000;
+}
+.video-box {
+  width: 100%;
 }
 </style>
